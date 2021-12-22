8bd704025da0b9a57209fce7a070ff936c94e570
jlb14n <jlb14n@gmail.com>
2021-12-22 14:45:58 -0500

    Part 1 completed

---
+++ FoodFiles.py
@@ -0,0 +1,5 @@
+#Part 1: Read in The Data
+foods=open("foods.txt","r").read().splitlines()
+highfiber=open("highfiber.txt","r").read().splitlines()
+lowfat=open("lowfat.txt","r").read().splitlines()
+low_glycemic_index=open("low-glycemic-index.txt").read().splitlines()
